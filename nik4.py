30e50f81f2d3a6fdbc730880df5de22d6db6a544
Michael Reichert <michael.reichert@geofabrik.de>
2018-02-14 15:20:52 +0100

    remove unnecessary early definition of variables

--- nik4.py
+++ nik4.py
@@ -20,10 +20,8 @@ IM_MONTAGE = 'montage'
 EPSG_4326 = '+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs'
 EPSG_3857 = '+proj=merc +a=6378137 +b=6378137 +lat_ts=0.0 +lon_0=0.0 +x_0=0.0 +y_0=0 +k=1.0 +units=m +nadgrids=@null +no_defs +over'
 
-proj_target = mapnik.Projection(EPSG_3857)
 proj_lonlat = mapnik.Projection(EPSG_4326)
 proj_web_merc = mapnik.Projection(EPSG_3857)
-transform = mapnik.ProjTransform(proj_lonlat, proj_target)
 transform_lonlat_webmerc = mapnik.ProjTransform(proj_lonlat, proj_web_merc)
 
 def layer_bbox(m, names, bbox=None):
