565397e1199f3b78807e0477d82e92102359a0bd
Michael Reichert <michael.reichert@geofabrik.de>
2018-06-27 16:07:46 +0200

    add more unit tests for size and scale calculations

--- tests/test_map_setup.py
+++ tests/test_map_setup.py
@@ -55,3 +55,83 @@ class MapSettingsTestCase(unittest.TestCase):
     def test_only_center_scale_ppi(self):
         settings = self.get_settings('-c 8.0327 49.0748 --scale 25000 --ppi 90')
         self.assertIsNone(settings.bbox)
+
+    def test_bbox_scale_ppi(self):
+        settings = self.get_settings('-b 8.0327 49.0748 8.0828 49.1049 --scale 25000 --ppi 90')
+        self.assertFalse(settings.need_cairo)
+        self.assertAlmostEqual(settings.bbox[0], 894196.07369513)
+        self.assertAlmostEqual(settings.bbox[1], 6287562.93266751)
+        self.assertAlmostEqual(settings.bbox[2], 899773.18018387)
+        self.assertAlmostEqual(settings.bbox[3], 6292679.50961837)
+        self.assertEqual(settings.proj_target.expanded(), WEB_MERC)
+        self.assertAlmostEqual(settings.scale, 10.7722048292)
+        # size of the image cannot be checked here because it is calculated later
+        #self.assertEqual(settings.size, [518, 475])
+        self.assertAlmostEqual(settings.scale_factor, 0.992282249173)
+        self.assertEqual(settings.fmt, 'png')
+
+    def test_center_scale_ppi_pixels(self):
+        settings = self.get_settings('-c 8.0327 49.0748 --scale 25000 --ppi 90 -x 400 600')
+        self.assertAlmostEqual(settings.bbox[0], 892042.28552930)
+        self.assertAlmostEqual(settings.bbox[1], 6284332.25041877)
+        self.assertAlmostEqual(settings.bbox[2], 896349.86186095)
+        self.assertAlmostEqual(settings.bbox[3], 6290793.61491624)
+        self.assertEqual(settings.proj_target.expanded(), WEB_MERC)
+        self.assertAlmostEqual(settings.scale, 10.7689408291)
+        self.assertEqual(settings.size, [400, 600])
+        self.assertAlmostEqual(settings.scale_factor, 0.992282249173)
+
+    def test_center_scale_ppi_mm(self):
+        settings = self.get_settings('-c 8.0327 49.0748 --scale 25000 --ppi 90 -d 400 600')
+        self.assertAlmostEqual(settings.bbox[0], 886566.27911769)
+        self.assertAlmostEqual(settings.bbox[1], 6276115.54856614)
+        self.assertAlmostEqual(settings.bbox[2], 901825.86827256)
+        self.assertAlmostEqual(settings.bbox[3], 6299010.31676887)
+        self.assertEqual(settings.proj_target.expanded(), WEB_MERC)
+        self.assertAlmostEqual(settings.scale, 10.7689408291)
+        self.assertEqual(settings.size, [1417, 2126])
+        self.assertAlmostEqual(settings.scale_factor, 0.992282249173)
+
+    def test_center_scale_300ppi_mm(self):
+        settings = self.get_settings('-c 8.0327 49.0748 --scale 25000 --ppi 300 -d 400 600')
+        self.assertAlmostEqual(settings.bbox[0], 886565.20222361)
+        self.assertAlmostEqual(settings.bbox[1], 6276115.01011910)
+        self.assertAlmostEqual(settings.bbox[2], 901826.94516665)
+        self.assertAlmostEqual(settings.bbox[3], 6299010.85521591)
+        self.assertEqual(settings.proj_target.expanded(), WEB_MERC)
+        self.assertAlmostEqual(settings.scale, 3.23068224874)
+        self.assertEqual(settings.size, [4724, 7087])
+        self.assertAlmostEqual(settings.scale_factor, 3.30760749724)
+
+    def test_bbox_pixels(self):
+        settings = self.get_settings('-b 8.0327 49.0748 8.0828 49.1049 -x 400 600')
+        self.assertAlmostEqual(settings.bbox[0], 894196.07369513)
+        self.assertAlmostEqual(settings.bbox[1], 6287562.93266751)
+        self.assertAlmostEqual(settings.bbox[2], 899773.18018387)
+        self.assertAlmostEqual(settings.bbox[3], 6292679.50961837)
+        self.assertEqual(settings.proj_target.expanded(), WEB_MERC)
+        self.assertIsNone(settings.scale)
+        self.assertEqual(settings.size, [400, 600])
+        self.assertEqual(settings.scale_factor, 1)
+
+    def test_bbox_pixels_sf3(self):
+        settings = self.get_settings('-b 8.0327 49.0748 8.0828 49.1049 -x 400 600 --factor 3')
+        self.assertAlmostEqual(settings.bbox[0], 894196.07369513)
+        self.assertAlmostEqual(settings.bbox[1], 6287562.93266751)
+        self.assertAlmostEqual(settings.bbox[2], 899773.18018387)
+        self.assertAlmostEqual(settings.bbox[3], 6292679.50961837)
+        self.assertIsNone(settings.scale)
+        self.assertEqual(settings.size, [400, 600])
+        self.assertEqual(settings.scale_factor, 3.0)
+
+    def test_bbox_pixels_300ppi(self):
+        settings = self.get_settings('-b 8.0327 49.0748 8.0828 49.1049 -x 400 600 --ppi 300')
+        self.assertFalse(settings.need_cairo)
+        self.assertAlmostEqual(settings.bbox[0], 894196.07369513)
+        self.assertAlmostEqual(settings.bbox[1], 6287562.93266751)
+        self.assertAlmostEqual(settings.bbox[2], 899773.18018387)
+        self.assertAlmostEqual(settings.bbox[3], 6292679.50961837)
+        self.assertEqual(settings.proj_target.expanded(), WEB_MERC)
+        self.assertIsNone(settings.scale)
+        self.assertEqual(settings.size, [400, 600])
+        self.assertAlmostEqual(settings.scale_factor, 3.30760749724)
